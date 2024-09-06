29ea08293ccc55e0260429656bf44af722d607ca
Darius Lauran <laurandarius@gmail.com>
2024-09-06 08:10:42 +0300

    add suspense to get rid of error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from 'next';
+import { Suspense } from 'react';
+import { GridLoader } from 'react-spinners';
+
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -13,7 +16,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="h-[100vh] w-[100vw]">{children}</body>
+      <body className="h-[100vh] w-[100vw]">
+        <Suspense
+          fallback={
+            <GridLoader
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              color="#000"
+              size={25}
+              aria-label="Loading Spinner"
+              loading
+            />
+          }
+        >
+          {children}
+        </Suspense>
+      </body>
     </html>
   );
 }
